4347de4651ced54a34eefe87d0922cf5e1e58cba
rajeev <rajeev.varshney@sugaldamani.com>
2018-11-13 12:33:22 +0530

    shopping list & recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
     new Recipe('A Test Recipe','This is simple test','https://www.cookingclassy.com/wp-content/uploads/2017/01/quinoa-vegetable-stew-7.jpg'),
-    new Recipe('A Test Recipe','This is simple test','https://www.cookingclassy.com/wp-content/uploads/2017/01/quinoa-vegetable-stew-7.jpg')        
+    new Recipe('Another Test Recipe','This is simple test','https://www.cookingclassy.com/wp-content/uploads/2017/01/quinoa-vegetable-stew-7.jpg')        
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
